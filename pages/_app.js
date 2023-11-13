2b7602b77dda01838d44c892062bbe392dbefce4
khs <khs1305@dreamsecurity.com>
2023-11-13 11:21:16 +0900

    [section01-01] example

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-
+// 모든 페이지의 공통설정들 진행
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
---
+++ pages/section01/01-01-example/index.js
@@ -0,0 +1,14 @@
+function board(){
+    // js 작성 공간
+    const aaa = 3;
+
+    // html 작성 공간
+    return(
+        <div>
+            <div>test</div>
+            <button>버튼</button>
+        </div>
+    )
+}
+
+export default board;
