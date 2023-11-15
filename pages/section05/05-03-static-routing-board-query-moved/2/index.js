484ccf7fc07f10fdd0bb920a3b25aa67748953fe
khs <khs1305@dreamsecurity.com>
2023-11-15 10:55:50 +0900

    [section05-03] router - 페이지 이동 후 게시글 조회

---
+++ pages/section05/05-03-static-routing-board-query-moved/2/index.js
@@ -0,0 +1,28 @@
+import { gql, useQuery } from "@apollo/client"
+
+const FETCH_BOARD = gql`
+    query{
+        fetchBoard(number:19592){
+            number
+            writer
+            title
+            contents
+        }
+    }
+`
+
+export default function StaticRoutingBoardQueryMovedPage(){
+    const { data } = useQuery(FETCH_BOARD)
+
+    console.log(data)
+
+    return (
+        <div>
+            <div>2번 페이지 이동 완료</div>
+            {/* 조건부 렌더링 대신 옵셔널 체이닝 사용 (optional-chaining)*/}
+            <div>작성자 : {data?.fetchBoard.writer}</div>
+            <div>제목 : {data?.fetchBoard.title}</div>
+            <div>내용 : {data?.fetchBoard.contents}</div>
+        </div>
+    )
+}
