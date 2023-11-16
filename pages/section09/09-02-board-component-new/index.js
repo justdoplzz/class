e0000f43568a006b445c8c75668d14c94c1e4daf
khs <khs1305@dreamsecurity.com>
2023-11-16 09:59:13 +0900

    [section09-02] board - component 분리 - component 에서 문자열로 받는 것 보다는 3항 연산자 처리가 낫다

---
+++ pages/section09/09-02-board-component-new/index.js
@@ -0,0 +1,9 @@
+import BoardComponent from "../../../src/components/units/board/09-component";
+
+export default function BoardNewPage(){
+    return(
+        <BoardComponent isEdit={false}>
+
+        </BoardComponent>
+    )
+}
