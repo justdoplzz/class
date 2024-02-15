06d0da8a43be3e9290b0b44f011be474f2bcf24a
khs <justdoplzz@gmail.com>
2024-02-15 17:53:25 +0900

    [js] 글씨 3번 연속 클릭 시 alert 뜸

--- pages/khs2/index.jsx
+++ pages/khs2/index.jsx
@@ -1,21 +1,43 @@
+import { useState } from 'react';
+
 const index = () => {
+
+    const [clickCount, setClickCount] = useState(0);
+    
+    console.log("clickCount",clickCount)
+    const handle3Click = () => {
+        setClickCount((prevCount) => prevCount + 1);
+
+        if(clickCount === 2) {
+            alert('3번 클릭함')
+            setClickCount(0);
+        }
+    }
+
     return(
-        <div style={{ display: 'flex', flexDirection: 'column'}}>
-            <div style={{ display: 'flex' , gap: '10px'}}>
-                <div style={{background: 'yellow', width: '100px'}}>
-                    a
+        <>
+            <div style={{ display: 'flex', flexDirection: 'column'}}>
+                <div style={{ display: 'flex' , gap: '10px'}}>
+                    <div style={{background: 'yellow', width: '100px'}}>
+                        a
+                    </div>
+                    <div style={{background: 'red', width: '100px'}}>
+                        b
+                    </div>
+                    <div style={{background: 'blue', width: '100px'}}>
+                        c
+                    </div>
                 </div>
-                <div style={{background: 'red', width: '100px'}}>
-                    b
-                </div>
-                <div style={{background: 'blue', width: '100px'}}>
-                    c
+                <div style={{background: 'green', width: '300px'}}>
+                    d
                 </div>
             </div>
-            <div style={{background: 'green', width: '300px'}}>
-                d
-            </div>
-        </div>
+            <br/><br/><br/>
+
+
+            
+            <h3 onClick={handle3Click} style={{background: 'pink'}}>3번 연속 클릭 시 alert 뜸</h3>
+        </>
     );
 }
 
