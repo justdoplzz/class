5f01d95f879739fbdf8f16babaa392081f960639
khs <justdoplzz@gmail.com>
2024-02-23 13:51:44 +0900

    [에러처리방법 - 2, 3] 에러 객체의 stack 속성을 사용하여 에러발생 위치 확인, 에러 메시지에 따른 특정 처리 수행 가능

--- pages/khs3-howToError/index.jsx
+++ pages/khs3-howToError/index.jsx
@@ -8,7 +8,18 @@ const index = () => {
             firstFunction();
             console.log("[index] firstFunction 호출 후")
         } catch (error) {
-            console.error("Error in Index: ", error.message)
+            // 에러 객체의 stack 속성을 사용하여 에러 발생 위치 확인
+            // console.error("Error in Index: ", error.stack);
+            console.error("Error in Index: ", error.message);
+
+            // 에러 메시지에 따라서 특정 처리를 수행할 수 있음
+            if(error.message.includes("firstFunction")){
+                console.error('Error in 3')
+            } else if (error.message.includes("secondFunction")){
+                console.error('Error in 2')
+            } else if (error.message.includes("thirdFunction")){
+                console.error('Error in 3')
+            }
         }
     }
 
